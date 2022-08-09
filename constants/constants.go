98524c8abc31651be12cf1859edf75e4657781ca
R-o-d-r-i-g-o <rodrigo.ribeiro@q2pay.com.br>
2022-08-09 16:14:50 -0300

    feat: adding gorm dependences

--- constants/constants.go
+++ constants/constants.go
@@ -4,8 +4,14 @@ const (
 
 	// Database table names
 	TB_USER            = "tb_user"
-	TB_PASTOR          = "tb_pastor"
 	TB_INDISPONIBILITY = "tb_indisponibility"
-
-	//
+	TB_USER_X_PASTOR   = "tb_user_x_pastor"
+	TB_OCCUPATION      = "tb_occupation"
+	TB_PASTOR          = "tb_pastor"
+	TB_PASTOR_CONFIGS  = "tb_pastor_configs"
+	TB_MONTH_SCHEDULE  = "tb_month_schedule"
+	TB_CELEBRATION_DAY = "tb_celebration_day"
+	TB_DAILY_DUTY      = "tb_daily_duty"
+	TB_DAY_SCHEDULE    = "tb_day_schedule"
+	TB_DUTY_GROUP      = "tb_duty_group"
 )
--- database/model/tables.go
+++ database/model/tables.go
@@ -5,6 +5,11 @@ import (
 	"time"
 )
 
+type DataBaseTables struct {
+}
+
+// ---------------------------------< 01º Table >--------------------------------- \\
+
 type User struct {
 	Id       uint64
 	Name     string //`gorm:"index"`
@@ -14,10 +19,23 @@ type User struct {
 	Indisp   []Indisponibility `gorm:"foreignKey:id_user;references:id"`
 }
 
-type Indisponibility struct {
-	Id           uint64
-	YearMonthDay time.Time `json:"year_month_day" gorm:"primaryKey"`
+func (User) TableName() string {
+	return constants.TB_USER
 }
+
+// ---------------------------------< 02º Table >--------------------------------- \\
+
+type Indisponibility struct {
+	YearMonthDay time.Time `json:"year_month_day  gorm:"column:year_month_day;primaryKey"`
+	UserId       uint64    `json:"user_id" 		  gorm:"column:user_id;primaryKey"`
+	NotPresent   bool      `json:"not_present" 	  gorm:"column:not_present"`
+}
+
+func (Indisponibility) TableName() string {
+	return constants.TB_INDISPONIBILITY
+}
+
+// ---------------------------------< 03º Table >--------------------------------- \\
 
 type User_x_Pastor struct {
 	UserId       uint64 `json:"id_user" gorm:"primaryKey"`
@@ -25,11 +43,23 @@ type User_x_Pastor struct {
 	OccupationId uint64 `json:"id_occupation" gorm:"primaryKey"`
 }
 
+func (User_x_Pastor) TableName() string {
+	return constants.TB_USER_X_PASTOR
+}
+
+// ---------------------------------< 04º Table >--------------------------------- \\
+
 type Occupation struct {
 	Id     uint64
 	Name   string
 	Us_x_P User_x_Pastor
 }
+
+func (Occupation) TableName() string {
+	return constants.TB_OCCUPATION
+}
+
+// ---------------------------------< 05º Table >--------------------------------- \\
 
 type Pastor struct {
 	Id           uint64
@@ -39,6 +69,12 @@ type Pastor struct {
 	CreationDate int
 }
 
+func (Pastor) TableName() string {
+	return constants.TB_PASTOR
+}
+
+// ---------------------------------< 06º Table >--------------------------------- \\
+
 type PastorConfigs struct {
 	PastorId             uint64
 	OrderByExperience    bool
@@ -46,12 +82,24 @@ type PastorConfigs struct {
 	ManualConfigInterval bool
 }
 
+func (PastorConfigs) TableName() string {
+	return constants.TB_PASTOR_CONFIGS
+}
+
+// ---------------------------------< 07º Table >--------------------------------- \\
+
 type MonthSchedule struct {
 	YearMonth      time.Time
 	PastorId       uint64
 	IntervalInDays uint64
 	Warning        string
 }
+
+func (MonthSchedule) TableName() string {
+	return constants.TB_MONTH_SCHEDULE
+}
+
+// ---------------------------------< 08º Table >--------------------------------- \\
 
 type CelebrationDay struct {
 	YearMonthDay time.Time
@@ -61,12 +109,24 @@ type CelebrationDay struct {
 	LastEditorId uint64
 }
 
-type dailyDuty struct {
+func (CelebrationDay) TableName() string {
+	return constants.TB_CELEBRATION_DAY
+}
+
+// ---------------------------------< 09º Table >--------------------------------- \\
+
+type DailyDuty struct {
 	YearMonthDay time.Time
 	Hour         time.Time
 	PastorId     uint64
 	NumOfMembers uint64
 }
+
+func (DailyDuty) TableName() string {
+	return constants.TB_DAILY_DUTY
+}
+
+// ---------------------------------< 10º Table >--------------------------------- \\
 
 type DaySchedule struct {
 	YearMonthDay time.Time
@@ -76,25 +136,23 @@ type DaySchedule struct {
 	GroupId      uint64
 }
 
+func (DaySchedule) TableName() string {
+	return constants.TB_DAY_SCHEDULE
+}
+
+// ---------------------------------< 11º Table >--------------------------------- \\
+
 type DutyGroup struct {
 	GroupId uint64
 	UserId  uint64
 }
 
+func (DutyGroup) TableName() string {
+	return constants.TB_DUTY_GROUP
+}
+
+// -----------------------------< General Interface >----------------------------- \\
+
 type Tabler interface {
 	TableName() string
-}
-
-// ---------------------------------< Methods to name database tables >--------------------------------- \\
-
-func (User) TableName() string {
-	return constants.TB_USER
-}
-
-func (Indisponibility) TableName() string {
-	return constants.TB_INDISPONIBILITY
-}
-
-func (Pastor) TableName() string {
-	return constants.TB_PASTOR
 }
