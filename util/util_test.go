f500b8e7c6f058d16b7551d39a538265b21e5fe1
R-o-d-r-i-g-o <rodrigo.ribeiro@q2pay.com.br>
2022-08-17 10:44:24 -0300

    feat: add tests in util functions

--- util/util_test.go
+++ util/util_test.go
@@ -1,5 +1,61 @@
 package util
 
 import (
-	_ "github.com/stretchr/testify/assert"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
+
+func TestParseMapToJson(t *testing.T) {
+
+	testMap := map[string]string{
+		"rodolfo": "handsome",
+		"skill":   "nice",
+	}
+
+	t.Run("ParseMapToJson --> must parse a string map", func(t *testing.T) {
+
+		expectedJsonElement := `{"rodolfo":"handsome","skill":"nice"}`
+
+		receivedJsonElement := ParseMapToJson(testMap)
+
+		assert.Equal(t, expectedJsonElement, receivedJsonElement)
+
+	})
+
+}
+func TestTrimAllSpacesInString(t *testing.T) {
+
+	t.Run("verify if the func is usefull to trim all spaces", func(t *testing.T) {
+
+		recivedBrokenString := TrimAllSpacesInString("C@l%i*e    n(tA   ut#he*  n t!  ica $tio$%nTo@  ken    ")
+
+		expectedCleanString := "C@l%i*en(tAut#he*nt!ica$tio$%nTo@ken"
+
+		assert.Equal(t, expectedCleanString, recivedBrokenString)
+	})
+}
+
+func TestRevomeSpecialChars(t *testing.T) {
+
+	t.Run("verify if regex trim all non-letter character", func(t *testing.T) {
+
+		recivedBrokenString := RevomeSpecialChars("C@l%i*en(tAut#he*nt!ica$tio$%nTo@kenáç")
+
+		expectedOnlyletters := "ClientAuthenticationToken"
+
+		assert.Equal(t, expectedOnlyletters, recivedBrokenString)
+	})
+}
+
+func TestLetOnlyNumbers(t *testing.T) {
+
+	t.Run("verify if regex trim all non-number character", func(t *testing.T) {
+
+		recivedBrokenString := LetOnlyNumbers("1g2h3#4m5n6v7c8U9$10")
+
+		expectedOnlyNumbers := "12345678910"
+
+		assert.Equal(t, expectedOnlyNumbers, recivedBrokenString)
+	})
+}
