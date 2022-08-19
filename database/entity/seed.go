92880a98d247b9eb11c6fc24fd66ed81414bdacc
R-o-d-r-i-g-o <rodrigomarqribeiro@gmail.com>
2022-08-19 20:40:00 -0300

    fix (seed): solving static table auth insert

--- database/entity/seed.go
+++ database/entity/seed.go
@@ -1,26 +1,23 @@
 package entity
 
 import (
-	"scheduler/library"
-
 	"gorm.io/gorm"
 )
 
-func Handler(db *gorm.DB) {
+var occupations = []Occupation{
+	{Id: 1, Name: "Leader of pastor(s)"},
+	{Id: 2, Name: "experienced member"},
+	{Id: 3, Name: "non-experienced member"},
+}
 
+func Handler(db *gorm.DB) {
 	populateTableOccupation(db)
 }
 
 func populateTableOccupation(db *gorm.DB) {
-	occupations := []Occupation{
-		{Id: 1, Name: "Leader of pastor(s)"},
-		{Id: 2, Name: "experienced member"},
-		{Id: 3, Name: "non-experienced member"},
-	}
-
 	for _, occupation := range occupations {
-		if db.Table(library.TB_OCCUPATION).Find(&occupation).Error != nil {
-			db.Table(library.TB_OCCUPATION).Create(&occupation)
+		if db.Create(&occupation).Error != nil {
+			break
 		}
 	}
 }
